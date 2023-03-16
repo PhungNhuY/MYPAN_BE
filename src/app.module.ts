e7922a5120140eaec5465b1b20347c3a4a314b95
PhungNhuY <ypn@tokyotechlab.com>
2023-03-16 22:14:53 +0700

    add env, mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        MongooseModule.forRoot(process.env.DB_LINK),
+    ],
     controllers: [AppController],
-    providers: [AppService],
 })
 export class AppModule {}
