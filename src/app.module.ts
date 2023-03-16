ce84a930e12decd505a90d4e273d62d995f5701d
PhungNhuY <ypn@tokyotechlab.com>
2023-03-16 22:47:50 +0700

    update imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PostModule } from './post/post.module';
 
 @Module({
     imports: [
@@ -9,6 +12,9 @@ import { MongooseModule } from '@nestjs/mongoose';
             isGlobal: true,
         }),
         MongooseModule.forRoot(process.env.DB_LINK),
+        UserModule,
+        AuthModule,
+        PostModule,
     ],
     controllers: [AppController],
 })
