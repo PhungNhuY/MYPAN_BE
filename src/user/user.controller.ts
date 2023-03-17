93faf92b7ae740e7a76e025aba394f3dac86fdbb
PhungNhuY <ypn@tokyotechlab.com>
2023-03-17 09:16:44 +0700

    init jwt authentication

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtAuthenticationGuard } from 'src/auth/jwt-authentication.guard';
 
 @Controller('user')
 export class UserController {
 
     @Get()
+    @UseGuards(JwtAuthenticationGuard)
     async getMe(@Req() req){
         return 'the data';
     }
