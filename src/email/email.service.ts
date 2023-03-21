248bef7fbbcb65481ae1a69216ff81ade9a15c12
PhungNhuY <yphung282@gmail.com>
2023-03-21 08:51:15 +0700

    catch error mail service

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -7,18 +7,22 @@ export class EmailService {
     constructor(private mailerService: MailerService) {}
 
     async sendUserConfirmation(email: string, token: string) {
-        const url = `${process.env.DOMAIN}/auth/confirm?token=${token}`;
+        try {
+            const url = `${process.env.DOMAIN}/auth/confirm?token=${token}`;
 
-        await this.mailerService.sendMail({
-            to: email,
-            // from: '"Support Team" <support@example.com>', // override default from
-            subject: `Welcome to ${process.env.APP_NAME}! Confirm your Email`,
-            template: './confirmation', // `.hbs` extension is appended automatically // ???
-            context: { // ✏️ filling curly brackets with content
-                name: email,
-                url,
-            },
-        });
-        console.log(email, token);
+            await this.mailerService.sendMail({
+                to: email,
+                // from: '"Support Team" <support@example.com>', // override default from
+                subject: `Welcome to ${process.env.APP_NAME}! Confirm your Email`,
+                template: './confirmation', // `.hbs` extension is appended automatically // ???
+                context: { // ✏️ filling curly brackets with content
+                    name: email,
+                    url,
+                },
+            });
+            console.log(`sent to ${email}`);
+        } catch (error) {
+            console.log('--->>> mailing service error: ', error);
+        }
     }
 }
