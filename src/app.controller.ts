d78a02267e632fdf73f78da06ec0ab09fa711365
PhungNhuY <yphung282@gmail.com>
2023-03-31 16:23:24 +0700

    add expample api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get } from '@nestjs/common';
+import { buildSuccessResponse } from './common/custom-response';
 
 @Controller()
 export class AppController {
@@ -6,5 +7,15 @@ export class AppController {
     @Get()
     getHello(): string {
         return 'Welcome to CookWithMe api';
+    }
+
+    @Get('success')
+    successResponse(){
+        return buildSuccessResponse({});
+    }
+
+    @Get('error')
+    errorResponse(){
+        throw new BadRequestException(['list', 'of', 'error', 'message']);
     }
 }
